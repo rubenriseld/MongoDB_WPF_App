9ece0dc69f9b89bd0d2cc265eaa7787dfee4d8bc
rubenriseld <rubenriseld@outlook.com>
2023-01-18 12:54:36 +0100

    base data access class
    properties and method for checking connection

---
+++ MongoDataAccess/DataAccess/DataAccess.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Core.Clusters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDataAccess.DataAccess;
+
+public class DataAccess
+{
+    private string? ConnectionString;
+    private const string DatabaseName = "gallery_db";
+
+    private const string ArtworkCollection = "artworks";
+    private const string CustomerCollection = "customers";
+
+    public void SetConnectionString(string connectionString)
+    {
+        ConnectionString = connectionString;
+    }
+    public async Task<bool> CheckConnection()
+    {
+        try
+        {
+            var client = new MongoClient(ConnectionString);
+            var databases = client.ListDatabasesAsync().Result;
+            //databases.MoveNextAsync();
+            if (client.Cluster.Description.State == ClusterState.Connected)
+            {
+                return true;
+            }
+            
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+        return false;
+    }
+}
