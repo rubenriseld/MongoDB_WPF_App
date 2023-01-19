c574986e6b59e519267edf5254d285067719ed93
rubenriseld <rubenriseld@outlook.com>
2023-01-19 16:40:46 +0100

    removed unnessecary inheritance

--- MongoDataAccess/Models/ArtworkModel.cs
+++ MongoDataAccess/Models/ArtworkModel.cs
@@ -4,12 +4,12 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace MongoDataAccess.Models;
 
-public class ArtworkModel : IModel
+public class ArtworkModel
 {
     [BsonId]
     public ObjectId Id { get; set; }
     [BsonElement("index")]
-    public int Index { get; set; }
+    public int? Index { get; set; }
 
     [BsonElement("title")]
     public string Title { get; set; }
